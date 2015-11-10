4876e7bebf86efb11b3a2af18461db38b738a145
alejandro barajas <alejandro.barajas@eagles.ewu.edu>
2015-11-09 18:26:22 -0800

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// REFER TO: 
+// click() : http://api.jquery.com/click/
+// addClass() : http://api.jquery.com/addClass/
+// removeClass() : http://api.jquery.com/removeClass/
+
+// On click Night Button....
+  $( "night-btn" ).click(function() {
+  alert( "night" );
+});
+  
+  // Remove Day Class
+
+  // Add Night Class
+    if( timeOfDay >= 13 ){
+$('body').addClass('night'); 
+
+
+
+// On click Day Button....
+
+  
+  // Remove Day Class
+  
+  // Add Night Class
+  
